5157593707c68443406237445a8b26ea5b66bf2d
Logan Severin <loganseverin@utexas.edu>
2021-10-18 15:12:01 -0500

    Initial commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Header from './HeaderComponent'
+
+class Main extends Component {
+    render() {
+        return (
+            <>
+                <Header />
+                <h1>This is the main component</h1>
+            </>
+        )
+    }
+}
+
+export default Main
